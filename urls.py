3c9d03c9c304d18cf14af3b4c365461115f33388
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-10-04 17:19:05 +0200

    nice css

--- urls.py
+++ urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,9 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/', include('static.urls')),
-        )
+
