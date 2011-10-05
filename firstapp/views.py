fd1d8ae37ac12b8c1d18fdcf3f4c628bb372f87f
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-10-05 17:03:25 +0200

    ticket: 3

--- firstapp/views.py
+++ firstapp/views.py
@@ -2,11 +2,11 @@
 
 from firstapp.models import PersonalInfo
 from django.shortcuts import render_to_response
-#from django.template import RequestContext
+
 
 
 def home(request, template_name="home.html"):
     data = PersonalInfo.objects.all()
     return render_to_response(template_name, {'data' : data})
-    #, context_instance=RequestContext(request))
+  
 
