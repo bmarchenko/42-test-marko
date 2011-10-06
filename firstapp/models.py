a2b048e103d46006353e23380223e9bc02d92050
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-10-06 15:32:32 +0200

    ticket: 5 finished

--- firstapp/models.py
+++ firstapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class PersonalInfo(models.Model):
     name = models.CharField('Name', max_length=50, blank=True, null=True)
     surname = models.CharField('Last Name', max_length=50, blank=True, null=True)
-    birthday = models.CharField('Date of birth', max_length=50, blank=True, null=True)
+    birthday = models.DateField('Date of birth', blank=True, null=True)
     bio = models.TextField('Bio', blank=True, null=True)
     email = models.EmailField('email', max_length=50, blank=True, null=True)
     jabber = models.CharField('Jabber', max_length=50, blank=True, null=True)
