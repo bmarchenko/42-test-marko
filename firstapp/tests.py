da4d6bb8dee6d27baea6d96596f68c3784055fd7
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-10-08 23:28:49 +0200

    ticket: 10 (10)

--- firstapp/tests.py
+++ firstapp/tests.py
@@ -4,6 +4,13 @@ except:
     import simplejson as json
 from django.test import TestCase
 
+
+
+class RequestTest(TestCase):
+    def test_requests(self):
+        response = self.client.get('/requests/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('request' in response.context)
 '''
 class HomePageTest(TestCase):
     
@@ -11,12 +18,6 @@ class HomePageTest(TestCase):
     def test_index(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('info' in response.context)
+        self.assertTrue('info' in response.context)'''
 
-
-class RequestTest(TestCase):
-    def test_requests(self):
-        response = self.client.get('/requests/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('request' in response.context)'''
 
