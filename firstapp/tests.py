130aa2e0498a036fa420137f8d1c6eef900b8a64
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-10-08 22:48:46 +0200

    ticket:10 (2)

--- firstapp/tests.py
+++ firstapp/tests.py
@@ -1,14 +1,8 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
 
 from django.test import TestCase
 
 class HomePageTest(TestCase):
-    fixtures = ['initial_data.json']
+   
 
     def test_index(self):
         response = self.client.get('/')
