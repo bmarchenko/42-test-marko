d36c520336c22ed7174d35eda6e9d003dde3c831
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-10-09 23:52:55 +0200

    ticket 10 final

--- firstapp/tests.py
+++ firstapp/tests.py
@@ -13,8 +13,8 @@ class HomePageTest(TestCase):
     def test_index(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('data' in response.context)
-        self.assertEqual([poll.pk for poll in response.context['data']], [1])
+        self.assertTrue('info' in response.context)
+        self.assertEqual([poll.pk for poll in response.context['info']], [1])
 
 class RequestTest(TestCase):
     def test_requests(self):
